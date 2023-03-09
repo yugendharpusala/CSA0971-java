6b362b0c7013636715e2edb478fcf350e735bdda
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-09 17:05:24 +0530

    Add files via upload

---
+++ 81.strtoint.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class strtoint
+{
+public static void main(String []args)
+{
+String S1;
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter the string");
+S1=Sc.nextLine();
+int S2=Integer.parseInt(S1);
+System.out.println(+S2);
+}
+}
