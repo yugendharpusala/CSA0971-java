6693b5d0b129fd3d2b2bae175f1eb7ec5d3c4ada
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-09 17:00:50 +0530

    Add files via upload

---
+++ 69.Integer_to_string_conversion[1].java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Intcon{  
+public static void main(String[] args)
+{ 
+  String s;
+ Scanner sc=new Scanner (System.in);
+ s=sc.next();
+int i=Integer.parseInt(s);  
+System.out.println(i);          
+    }  
+}
