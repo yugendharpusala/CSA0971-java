5eee2608c7044423d3ee4a916cc09a13977e87b6
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-09 17:00:06 +0530

    Add files via upload

---
+++ 67.decbinoct.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class decbinoct
+{
+public static void main(String[]args)
+{
+Scanner sc=new scanner(system.in);
+System.out.println("Enter the decimal number:");
+i=nextInt();
+j=Integer.parseInt(i);
+System.out.println("Integer.toBinarystring(i));
+k=Integer.parseInt(i);
+System.out.println(");
+}
+}
+
