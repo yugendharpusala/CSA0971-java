4072b1e8f74f6eb6c4bfb5eb7127611eb83d8a77
yugendharpusala <113416216+yugendharpusala@users.noreply.github.com>
2023-03-09 17:24:24 +0530

    Add files via upload

---
+++ 90.BinaryToDecimal.java
@@ -0,0 +1,9 @@
+public class BinaryToDecimal
+{  
+public static void main(String args[])
+{  
+System.out.println(Integer.parseInt("1010",2));  
+System.out.println(Integer.parseInt("111",7));  
+System.out.println(Integer.parseInt("11111",16));  
+}
+}  
